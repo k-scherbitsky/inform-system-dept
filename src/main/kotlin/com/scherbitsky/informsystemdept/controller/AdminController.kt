4f8ac575b9f6052a3106afbbe33cc670a31396e2
Konstantin Scherbitsky <k.scherbitsky@gmail.com>
2019-04-24 17:37:59 +0300

    create manage of teachers page & manage of subject page

--- src/main/kotlin/com/scherbitsky/informsystemdept/controller/AdminController.kt
+++ src/main/kotlin/com/scherbitsky/informsystemdept/controller/AdminController.kt
@@ -1,6 +1,8 @@
 package com.scherbitsky.informsystemdept.controller
 
 import com.scherbitsky.informsystemdept.dto.BindingDTO
+import com.scherbitsky.informsystemdept.dto.SubjectDTO
+import com.scherbitsky.informsystemdept.dto.UserDTO
 import com.scherbitsky.informsystemdept.repository.BindingRepository
 import com.scherbitsky.informsystemdept.repository.SubjectRepository
 import com.scherbitsky.informsystemdept.repository.UserRepository
@@ -23,10 +25,25 @@ class AdminController
     }
 
     @GetMapping("/binding")
-    fun removeBinding(model: Model): String {
+    fun bindingTable(model: Model): String {
         val bindingDtoList: List<BindingDTO> = bindingRepository.findAll().map { BindingDTO.toDto(it) }
         model.addAttribute("bindingDtoList", bindingDtoList)
-        return "admin/remove/binding"
+        return "admin/manage/binding"
     }
+
+    @GetMapping("/teacher")
+    fun teacherTable(model: Model): String {
+        val userDtoList: List<UserDTO> = userRepository.findAll().map { UserDTO.toDto(it) }
+        model.addAttribute("userDtoList", userDtoList)
+        return "admin/manage/teacher"
+    }
+
+    @GetMapping("/subject")
+    fun subjectTable(model: Model): String {
+        val subjectDtoList: List<SubjectDTO> = subjectRepository.findAll().map { SubjectDTO.toDto(it) }
+        model.addAttribute("subjectDtoList", subjectDtoList)
+        return "admin/manage/subject"
+    }
+
 
 }
